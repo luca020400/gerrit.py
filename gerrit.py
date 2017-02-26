f2a46e18561014eb48cbf52a0917b870bf81f671
Luca Stefani <luca020400@lineageos.org>
2017-02-26 18:45:24 +0100

    gerrit.py: Download compact json
    Change-Id: I34a1deb3c3ce1f53849c51b4709b962858865f6c

--- gerrit.py
+++ gerrit.py
@@ -100,7 +100,7 @@ def main():
 
     if options.topic:
         print('Fetching topic changes')
-        response = requests.get(url + "?q=topic:" + options.topic, auth=auth)
+        response = requests.get(url + "?pp=0q&=topic:" + options.topic, auth=auth)
         if response.status_code != 200:
             sys.exit("Could not fetch topic changes")
         else:
@@ -117,7 +117,7 @@ def main():
 
     for c in changes:
         try:
-            response = requests.get(url + c + "/detail/", auth=auth)
+            response = requests.get(url + c + "/detail/?pp=0", auth=auth)
             if response.status_code != 200:
                 sys.exit("Could not fetch commit information")
             else:
